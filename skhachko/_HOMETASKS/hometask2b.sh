1e7934d3263cc7496e8626f5a63bc758d0ed9a9d
skhachko <skhachko@cogniance.com>
2011-11-08 12:32:49 +0200

    hometask2a improved

--- skhachko/_HOMETASKS/hometask2b.sh
+++ skhachko/_HOMETASKS/hometask2b.sh
@@ -1,1 +1,7 @@
-echo "a.aa...b.c....d..e" | sed 's/\.\./\./g' | sed 's/\.\./\./g'
+#Replace all runs of consecutive spaces with a single space. Thus "a.b" is unchanged and "a..b" becomes "a.b", using dot to make the space visible.
+
+echo -n "Enter the string to remove space-dublicates: "
+read var
+echo $var | sed 's/\.\./\./g' | sed 's/\.\./\./g'
+echo "Done... Press any key"
+read
