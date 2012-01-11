6358c8337daa6ccab3c4507c4c29b586b097f67c
maxvasilchuk <mvasilchuk@cogniance.com>
2012-01-11 12:58:16 +0200

    Update mvasilchuk/selenium2-cogniance-tutorial/2-OOP approach/features/support/env.rb

--- mvasilchuk/selenium2-cogniance-tutorial/2-OOP approach/features/support/env.rb
+++ mvasilchuk/selenium2-cogniance-tutorial/2-OOP approach/features/support/env.rb
@@ -8,7 +8,7 @@ browser.manage.timeouts.implicit_wait = 10 # seconds
 
 Before do
    $browser=browser
-  #$ - pointing, that variable is accessible from any class, part of project
-  # instead of @, pointing, variable should be invoke by using constructor of class: myclass.new(@browser)
+  #$ - pointing, that variable is accessible from any class, part of project. In other words: variable is global
+  #instead of $, @ variable should be invoke by using constructor of class: myclass.new(@browser)
 
 end
